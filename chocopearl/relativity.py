2f6907772946a967006b52928a3b8b11e790f8f0
Carlos Francisco García Hipólito <cfgarcia1903@gmail.com>
2023-12-22 17:11:07 -0500

    Update relativity.py

--- chocopearl/relativity.py
+++ chocopearl/relativity.py
@@ -2,6 +2,8 @@ import numpy as np
 
 def calculate_gamma_beta_momentum(energy, mass):
     '''
+    calculate_gamma_beta_momentum(energy, mass) takes the energy and mass of a particle and outputs it's Lorentz Gamma Factor, Beta and relativistic momentum
+    
     Energy and mass must be in the same units (in natural units). For example, if energy is in GeV, mass must be in GeV. 
     The function outputs momentum also in that same units.
     '''
