cd9c5c544e2233e50dd40d9b7fa9c68a0db69e8c
Carlos Francisco García Hipólito <cfgarcia1903@gmail.com>
2024-01-12 20:25:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['numpy', 'warnings', 'pandas','datetime'],
+    install_requires=['numpy', 'pandas'],
     keywords=['python', 'physics', 'general', 'corsika', 'astroparticle', 'numerical methods'],
     classifiers=[
         "Development Status :: 1 - Planning",
