8f701172554da2883ceecc4e23a0d3592a3b7ca8
Carlos Francisco García Hipólito <cfgarcia1903@gmail.com>
2024-04-02 13:24:08 -0500

    Update __init__.py

--- chocopearl/__init__.py
+++ chocopearl/__init__.py
@@ -1,7 +1,8 @@
-from . import particle
+from . import corsika
 from . import general
 from . import nummath
 
-from .particle import *
+from .corsika import *
 from .general import *
 from .nummath import *
+from .relativity import *
