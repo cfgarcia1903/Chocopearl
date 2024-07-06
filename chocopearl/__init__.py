5f7761344c000f6d09947a8186b29736bebecd7d
Carlos Francisco García Hipólito <cfgarcia1903@gmail.com>
2024-07-06 16:13:49 -0500

    .

--- chocopearl/__init__.py
+++ chocopearl/__init__.py
@@ -1,8 +1,13 @@
 from . import corsika
 from . import general
 from . import nummath
+from . import relativity
+from . import osfunc
+#from . import dirac
 
 from .corsika import *
 from .general import *
 from .nummath import *
+from .osfunc import *
 from .relativity import *
+#from .dirac import *
